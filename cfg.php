fcf5fdf6e58ad07faa256a31fb37f6171c04144b
bioprozium <bioprozium@gmail.com>
2022-09-19 13:28:41 +0400

    Update: in cfg.php file created new lines {DEFINE some folders as...}

---
+++ cfg.php
@@ -0,0 +1,7 @@
+if(isset($_GET["session_id"]))
+	session_id($_GET["session_id"]);
+session_start();
+DEFINE("SESSION_ID", session_id());
+DEFINE ("HTML_PATH", $_SERVER["DOCUMENT_ROOT"]."/");
+DEFINE ("ROOT_PATH", str_replace("\\","/",__DIR__)."/");
+DEFINE ("CLASSES", ROOT_PATH."classes/");
